349df641b6442cb660293581fe08261fa4702bb2
Duy Huynh <vndee.huynh@gmail.com>
2023-09-03 21:29:55 +0700

    update several content

--- components/data.js
+++ components/data.js
@@ -1,60 +1,65 @@
 import {
-  FaceSmileIcon,
-  ChartBarSquareIcon,
-  CursorArrowRaysIcon,
-  DevicePhoneMobileIcon,
-  AdjustmentsHorizontalIcon,
-  SunIcon,
+	SunIcon,
+	FireIcon,
+	LanguageIcon,
+	ShieldCheckIcon,
+	RocketLaunchIcon,
+	DevicePhoneMobileIcon,
+	AdjustmentsHorizontalIcon,
 } from "@heroicons/react/24/solid";
 
 import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
-  desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
-  image: benefitOneImg,
-  bullets: [
-    {
-      title: "Understand your customers",
-      desc: "Then explain the first point breifly in one or two lines.",
-      icon: <FaceSmileIcon />,
-    },
-    {
-      title: "Improve acquisition",
-      desc: "Here you can add the next benefit point.",
-      icon: <ChartBarSquareIcon />,
-    },
-    {
-      title: "Drive customer retention",
-      desc: "This will be your last bullet point in this section.",
-      icon: <CursorArrowRaysIcon />,
-    },
-  ],
+	title: "Outstanding Features",
+	desc: "LensQuery offers a powerful application that helps you to understand and explore what you see.",
+	image: benefitOneImg,
+	bullets: [
+		{
+			title: "Instant Image Analysis",
+			desc: "Snap a picture and LensQuery's advanced algorithms immediately get to work. Whether it's a page of text, or a mathematical equation, LensQuery quickly analyzes the content to give you insights.",
+			icon: <FireIcon />,
+		},
+		{
+			title: "Engaging AI Conversations",
+			desc: "Have a question about what you've captured? Engage with our state-of-the-art AI chat interface. Get elaborative answers, seek clarifications, or simply explore the topic further.",
+			icon: <RocketLaunchIcon />,
+		},
+		{
+			title: "Multi-Lingual Support",
+			desc: "LensQuery isn't just limited to English. We understand the importance of global learning, which is why our app supports multiple languages. Snap, learn, and converse in your preferred language.",
+			icon: <LanguageIcon />,
+		},
+		{
+			title: "Privacy-Centric Design",
+			desc: "Your privacy is our utmost priority. LensQuery is designed with stringent data protection measures, ensuring your snaps and queries remain confidential.",
+			icon: <ShieldCheckIcon />,
+		},
+	],
 };
 
 const benefitTwo = {
-  title: "Offer more benefits here",
-  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
-  image: benefitTwoImg,
-  bullets: [
-    {
-      title: "Mobile Responsive Template",
-      desc: "Nextly is designed as a mobile first responsive template.",
-      icon: <DevicePhoneMobileIcon />,
-    },
-    {
-      title: "Powered by Next.js & TailwindCSS",
-      desc: "This template is powered by latest technologies and tools.",
-      icon: <AdjustmentsHorizontalIcon />,
-    },
-    {
-      title: "Dark & Light Mode",
-      desc: "Nextly comes with a zero-config light & dark mode. ",
-      icon: <SunIcon />,
-    },
-  ],
+	title: "Offer more benefits here",
+	desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+	image: benefitTwoImg,
+	bullets: [
+		{
+			title: "Mobile Responsive Template",
+			desc: "Nextly is designed as a mobile first responsive template.",
+			icon: <DevicePhoneMobileIcon />,
+		},
+		{
+			title: "Powered by Next.js & TailwindCSS",
+			desc: "This template is powered by latest technologies and tools.",
+			icon: <AdjustmentsHorizontalIcon />,
+		},
+		{
+			title: "Dark & Light Mode",
+			desc: "Nextly comes with a zero-config light & dark mode. ",
+			icon: <SunIcon />,
+		},
+	],
 };
 
-
-export {benefitOne, benefitTwo};
+export { benefitOne, benefitTwo };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,29 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        trueGray: colors.neutral,
-      },
-    },
-    fontFamily: {
-      sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      stock: [defaultTheme.fontFamily.sans],
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+	mode: "jit",
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	darkMode: "class", // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				trueGray: colors.neutral,
+				indigo: {
+					500: "#0000ff",
+					600: "#0000ff",
+				},
+			},
+		},
+		fontFamily: {
+			sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			stock: [defaultTheme.fontFamily.sans],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/aspect-ratio")],
 };
