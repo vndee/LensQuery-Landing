300a8fcf6b08ede157ff1b7a09bb8ac44063200a
Duy Huynh <vndee.huynh@gmail.com>
2023-10-04 00:19:35 +0700

    add app link

--- components/hero.js
+++ components/hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
 						</h1>
 						<p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
 							Unlock the power of modern AI models with LensQuery.
-							Chose your favorite model and play around.
+							Choose your favorite model and play around.
 						</p>
 
 						<div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
